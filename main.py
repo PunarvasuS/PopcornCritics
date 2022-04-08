33117637e5802ff0719d476fb46a84fb9df53a9f
PunarvasuS <punarvasu.sangam@gmail.com>
2022-04-07 23:23:26 -0700

    added full navbar + html pages (no content)

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/top')
+def top():
+    return render_template("top.html")
+
+@app.route('/random')
+def random():
+    return render_template("random.html")
+
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
+
+@app.route('/learn')
+def learn():
+    return render_template("learn.html")
+
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.run(debug=True, port="5222")
